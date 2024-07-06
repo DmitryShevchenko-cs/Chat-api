47e5675ee67452de97c3b0972cf3b59740205a63
Dmitry <dima.shevchenko10@gmail.com>
2024-07-06 20:57:01 +0300

    create api structure

---
+++ Chat.Web/Program.cs
@@ -0,0 +1,18 @@
+namespace Chat.Web;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureLogging((context, logging) =>
+            {
+                logging.AddDebug();
+                logging.AddConsole();
+            })
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+}
